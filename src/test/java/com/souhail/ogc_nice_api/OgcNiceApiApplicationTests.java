aa85341a5be9c07eb99f3c1a446b7728d90090dd
Souhail Zakar <souhailzakar@MacBook-Pro-de-Souhail.local>
2025-12-08 15:27:32 +0100

    test: configure H2 test profile and fix contextLoads

--- src/test/java/com/souhail/ogc_nice_api/OgcNiceApiApplicationTests.java
+++ src/test/java/com/souhail/ogc_nice_api/OgcNiceApiApplicationTests.java
@@ -2,12 +2,13 @@ package com.souhail.ogc_nice_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class OgcNiceApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
